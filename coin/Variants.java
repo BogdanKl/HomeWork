0bd366d86754be808cbd4fb1cf28324854c24fce
BogdanKl <ogdan@list.ru>
2012-10-12 16:45:59 +0400

    coin v.2

--- coin/Variants.java
+++ coin/Variants.java
@@ -1,15 +1,22 @@
 package coins;
 
 public class Variants {
-    public void copy(int [] a, int i, int[] b, int j ,int l){
-        for(int t = 0; t < l; t++ )
+    public int variantspart(int i,int needs, int[] a)
+    {
+        if (needs == 0)
         {
-            b[j+t] = a[i+t];
+            return 1;
         }
+        if ((i == (a.length-1))||(needs < 0))
+        {
+            return 0;       
+        }
+        return(variantspart(i+1, needs, a)+variantspart(i, needs - a[i], a));
+        
+        
     }
-    public int leng = 0;
-    public int variants(int need, int[] coin){
-        leng = coin.length;
+       public int variants(int need, int[] coin){
+        int leng = coin.length;
         if (need == 0)
         {
             return 1;
@@ -21,10 +28,8 @@ public class Variants {
         if (leng == 0)
         {
             return 0;
-        }
-        int[] temp = new int[leng-1];
-        copy(coin, 1, temp, 0, leng-1);
-        return (variants(need, temp) + variants(need - coin[0], coin));
+        }    
+        return (variantspart(1,need, coin) + variants(need - coin[0], coin));
         }
         
 }
